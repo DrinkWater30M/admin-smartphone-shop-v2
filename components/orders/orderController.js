60efb6ea84087d3b19c040d5c7b679eb1caed7b5
anhminh <nguyenanhminh04012001@gmail.com>
2022-01-16 15:37:21 +0700

    Tính tổng tiền order

--- components/orders/orderController.js
+++ components/orders/orderController.js
@@ -18,7 +18,7 @@ const getOrderList = async (req, res) => {
         let count = 1;
         let order = [];
         order[0] = orderList[i];
-        let j = i+1;
+        let j = i + 1;
         while (j < orderList.length) {
             if (orderList[i].MaKhachHang == orderList[j].MaKhachHang && orderList[i].MaDonHang == orderList[j].MaDonHang) {
                 order[count] = orderList[j];
@@ -28,20 +28,37 @@ const getOrderList = async (req, res) => {
             else
                 j++;
         }
+        let totalPrice = 0;
+        order.forEach(element => {
+            totalPrice += element.DonGia * element.SoLuongSanPham;
+        })
+        order[0].TongTien = totalPrice;
         orders[d] = order;
         console.log("🚀 ~ file: orderController.js ~ line 29 ~ getOrderList ~ order", order)
         d++;
     }
     console.log(orders)
+    const totalOrders = orders.length;
+    let willDeliver = 0;
+    let delivering = 0;
+    let delivered = 0
+    orders.forEach(element => {
+        if (element[0].TrangThaiDonHang == 0)
+            willDeliver++
+        else if (element[0].TrangThaiDonHang == 1)
+            delivering++
+        else if (element[0].TrangThaiDonHang == 2)
+            delivered++
+    });
     let i = pageNumber * itemPerPage;
-    res.render('orders/orderList', { currentPage: pageNumber + 1, order1: orders[i], order2: orders[i + 1], order3: orders[i + 2], order4: orders[i + 3] });
+    res.render('orders/orderList', { currentPage: pageNumber + 1, order1: orders[i], order2: orders[i + 1], order3: orders[i + 2], order4: orders[i + 3], totalOrders, willDeliver, delivering, delivered });
 
 }
 
 const handlingOrder = async (req, res) => {
     if (req.body.choose === 'update')
         await orderService.updateOrder(req.body.order_id, req.body.account_id);
-    else if (req.body.choose === 'del') 
+    else if (req.body.choose === 'del')
         await orderService.delOrder(req.body.order_id, req.body.account_id);
     await getOrderList(req, res);
 }
