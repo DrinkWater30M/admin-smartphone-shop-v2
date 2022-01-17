b47c5692d41b4a536ea39932bc4bff4dc687b7d0
anhminh <nguyenanhminh04012001@gmail.com>
2022-01-17 08:18:07 +0700

    thêm biểu đồ số đơn hàng

--- public/javascripts/chartDashboard.js
+++ public/javascripts/chartDashboard.js
@@ -1,4 +1,5 @@
 const canvas = document.getElementById('line-chart').getContext('2d');
+const chartOrder = document.getElementById('order-chart').getContext('2d');
 const orderPrice = document.getElementsByName('order-price');
 const orderNumber = document.getElementsByName('order-number');
 const data = document.getElementsByName('order-time');
@@ -15,7 +16,20 @@ function parseDataToMonths() {
     console.log(months);
     return months;
 }
-parseDataToMonths();
+function chartNumberOrders() {
+
+    let months = [];
+    for (let i = 0; i < 12; i++)
+    months[i] = 0;
+    for (let i = 0; i < data.length; i++) {
+        let time = data[i].defaultValue;
+        time = time.slice(5, 7);
+        months[parseInt(time, 10)-1] += orderNumber[i].defaultValue;
+    }
+    console.log(months);
+    return months;
+}
+
 let chart = new Chart(canvas, {
     type: 'bar',
     data: {
@@ -23,6 +37,20 @@ let chart = new Chart(canvas, {
         datasets: [{
             label: "vnđ",
             data: parseDataToMonths(),
+            backgroundColor: [
+                'blue'
+            ]
+        }],
+    }
+});
+
+let orderChart = new Chart(chartOrder, {
+    type: 'bar',
+    data: {
+        labels: ['1', '2', '3', '4', '5','6','7','8','9','10','11','12'],
+        datasets: [{
+            label: "số lượng",
+            data: chartNumberOrders(),
             backgroundColor: [
                 'blue'
             ]
