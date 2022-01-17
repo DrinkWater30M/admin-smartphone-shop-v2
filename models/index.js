97040e544d58c24cf92cae7f6ccd4ec7772b0b23
Nguyen Van Loi <nguyenvanloi10101@gmail.com>
2022-01-17 20:23:04 +0700

    change .env, config sequelize to connect

--- models/index.js
+++ models/index.js
@@ -1,14 +1,13 @@
-const Sequelize = require('sequelize');
+const  {Sequelize}  = require('sequelize');
+const initModels = require("./init-models");
 
-const initModels = require('./init-models');
-
-console.log(process.env.DB_DATABASE);
-const sequelize = new Sequelize(process.env.DB_DATABASE,process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
-    dialect: 'mysql',
+    port: process.env.DB_PORT,
+    dialect: 'mysql'
 });
 
 module.exports = {
     sequelize,
-    models: initModels(sequelize),
+    models: initModels(sequelize)
 };
